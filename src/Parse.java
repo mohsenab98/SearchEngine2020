eba9469f893f27717c1697205199b3ed1ae6c4a5
mohse <mohsen.abdalla.98@gmail.com>
2019-11-17 22:56:47 +0200

    Date, Percent Functions!

--- src/Parse.java
+++ src/Parse.java
@@ -41,36 +41,68 @@ public class Parse {
             return term;
     }
 
-    public String Price(String term){
-        if(term.contains("$")){
-            term = term.replace("$", "");
-            term = term + " Dollars";
+    public String NumWithPercent(String term){
+        String replacedStr = term;
+        if(term.contains("percentage")){
+            replacedStr  = term.replaceAll("percentage", "%");
+        }
+        else if(term.contains("percent")){
+            replacedStr = term.replaceAll("percent", "%");
+        }
+        replacedStr = replacedStr.replaceAll("[\\s]","");
+
+        return replacedStr;
+    }
+
+    /**
+     * 5th term rule // Dates
+     */
+    enum Mounth {january , february, march, april, may, june, july, august, september, october, november, december};
+    enum MountThreeChar {jan , feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec};
+    public String NumWithDates(String term){
+
+        String strWithDigitOnly = term.replaceAll("[\\D]","");
+        float numberInTerm = Float.parseFloat(strWithDigitOnly);
+        String strWithCharOnly  = term.replaceAll(strWithDigitOnly, "");
+        if(numberInTerm<10){
+            strWithDigitOnly = "0" + strWithDigitOnly ;
+        }
+        strWithCharOnly = strWithCharOnly.replaceAll("[\\s]","");
+
+        int monthNumber = monthContains(strWithCharOnly);
+        String monthNumberStr = String.valueOf(monthNumber);
+        if(monthNumber < 10){
+            monthNumberStr = "0" + monthNumber;
+        }
+        // Month Number Could be a year
+        if(numberInTerm <= 31){
+            return monthNumberStr+"-"+strWithDigitOnly;
+        }else {
+            return strWithDigitOnly+"-"+monthNumberStr;
+        }
+    }
+
+    /**
+     *
+     * @param test
+     * @return the number that represent each month
+     */
+    public int monthContains(String test) {
+        int i = 1;
+        for (Mounth m : Mounth.values()) {
+            if (m.name().equals(test.toLowerCase())) {
+                return i;
+            }
+            i++;
+        }
+        i=1;
+        for (MountThreeChar m : MountThreeChar.values()) {
+            if (m.name().equals(test.toLowerCase())) {
+                return i;
+            }
+            i++;
         }
 
-        if(term.contains("U.S.")){
-            term = term.replace("U.S.", "");
-        }
-
-        int indexAfterDot;
-        float numberInTerm = Float.parseFloat(term.replaceAll("[\\D]", ""));
-        if(term.contains(".")){
-            String numberInTermStr = numberInTerm + "";
-            indexAfterDot = numberInTermStr.length() - (numberInTermStr.indexOf(".") + 1);
-            numberInTerm = numberInTerm/(float)Math.pow(10, indexAfterDot);
-        }
-
-        if(numberInTerm >= 1000000){
-            return numberInTerm/1000000 + " M" + " Dollars";
-        }
-
-        if(term.contains("million") || term.contains("m ")){
-            return numberInTerm + " M" + " Dollars";
-        }
-
-        if(term.contains("billion") || term.contains("bn ")){
-            return numberInTerm * 1000 + " M" + " Dollars";
-        }
-
-        return term;
+        return -1;
     }
 }
