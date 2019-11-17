d0dbe2f3804a2997d18cae922fec64278f0f5424
Evgeniy Umanskiy <evgeniyu@sensus.co.il>
2019-11-17 23:00:27 +0200

    Price

--- src/Parse.java
+++ src/Parse.java
@@ -57,8 +57,8 @@ public class Parse {
     /**
      * 5th term rule // Dates
      */
-    enum Mounth {january , february, march, april, may, june, july, august, september, october, november, december};
-    enum MountThreeChar {jan , feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec};
+    enum Mounth {january , february, march, april, may, june, july, august, september, october, november, december}
+    enum MountThreeChar {jan , feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec}
     public String NumWithDates(String term){
 
         String strWithDigitOnly = term.replaceAll("[\\D]","");
@@ -104,5 +104,38 @@ public class Parse {
         }
 
         return -1;
+    }
+
+    public String Price(String term){
+        if(term.contains("$")){
+            term = term.replace("$", "");
+            term = term + " Dollars";
+        }
+
+        if(term.contains("U.S.")){
+            term = term.replace("U.S.", "");
+        }
+
+        int indexAfterDot;
+        float numberInTerm = Float.parseFloat(term.replaceAll("[\\D]", ""));
+        if(term.contains(".")){
+            String numberInTermStr = numberInTerm + "";
+            indexAfterDot = numberInTermStr.length() - (term.indexOf(".") + 1);
+            numberInTerm = numberInTerm/(float)Math.pow(10, indexAfterDot);
+        }
+
+        if(numberInTerm >= 1000000){
+            return numberInTerm/1000000 + " M" + " Dollars";
+        }
+
+        if(term.contains("million") || term.contains("m ")){
+            return numberInTerm + " M" + " Dollars";
+        }
+
+        if(term.contains("billion") || term.contains("bn ")){
+            return numberInTerm * 1000 + " M" + " Dollars";
+        }
+
+        return term;
     }
 }
