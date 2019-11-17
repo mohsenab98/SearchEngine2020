78c4c0a9f6e85bb3bf7c44353ab3926f46298803
mohse <mohsen.abdalla.98@gmail.com>
2019-11-17 20:29:33 +0200

    NumWithoutUnits Function Parser!

--- src/Parse.java
+++ src/Parse.java
@@ -1,2 +1,43 @@
 public class Parse {
+
+    public String NumWithoutUnits(String term){
+            int indexAfterDot;
+            float numberInTerm = Float.parseFloat(term.replaceAll("[\\D]", ""));
+            if(term.contains(".")){
+                indexAfterDot = term.length() - (term.indexOf(".") + 1);
+                numberInTerm = numberInTerm/(float)Math.pow(10, indexAfterDot);
+            }
+
+            int range = 0;
+            if((numberInTerm >= 1000 && numberInTerm < 1000000)){
+                range = 1;
+            }else if((numberInTerm >= 1000000 && numberInTerm < 1000000000)){
+                range = 2;
+            }else if(numberInTerm >= 1000000000 ){
+                range = 3;
+            }else if(term.contains("Thousand")){
+                range = 4;
+            }else if( term.contains("Million")){
+                range = 5;
+            }else if(term.contains("Billion")){
+                range = 6;
+            }
+
+            switch (range){
+                case 1:
+                    return String.format("%.03f",numberInTerm/1000) +"K";
+                case 2:
+                    return String.format("%.03f", numberInTerm/1000000) +"M";
+                case 3:
+                    return String.format("%.03f",numberInTerm/1000000000) +"B";
+                case 4:
+                    return numberInTerm +"K";
+                case 5:
+                    return numberInTerm +"M";
+                case 6:
+                    return numberInTerm +"B";
+            }
+
+            return term;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,3 +1,7 @@
 public class main {
     //ddddddddddddddd evgenyyyyyy
+    public static void main(String[] args){
+        Parse p = new Parse();
+        System.out.println(p.NumWithoutUnits("100000000000000"));
+    }
 }
