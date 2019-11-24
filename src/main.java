983bbfbfcb1a3b12ccdeae9a97a574337ab90935
mohse <mohsen.abdalla.98@gmail.com>
2019-11-24 20:08:45 +0200

    Parser Class: +Delete StopWords Func +2 Private Func that help delete the stopWords

--- src/main.java
+++ src/main.java
@@ -1,3 +1,5 @@
+import java.util.Set;
+
 public class main {
     //ddddddddddddddd evgenyyyyyy
     public static void main(String[] args){
@@ -6,18 +8,21 @@ public class main {
         String path = "Jackpot!";
         ReadFile rd = new ReadFile(path);
         System.out.println(rd.getPath());
-        rd.setPath("C:\\Users\\EvgeniyU\\Desktop\\ThirdYear\\DataRetrieval\\corpusTest");
+        rd.setPath("C:\\Users\\mohse\\Desktop\\corpusTest");
         System.out.println(rd.getPath());
         rd.printPath();
-
         rd.filesSeparator();
 
     /////// Parse tests ///////
         Parse p = new Parse(rd.getMapAllDocs());
-        p.Parser();
         //System.out.println(p.NumWithoutUnits("1010.56"));
         //System.out.println(p.NumWithPercent("15 percent"));
         //System.out.println(p.NumWithDates("15 May"));
         //System.out.println(p.Price("20.6m Dollars"));
+
+        // p.stringToSetOfString("a ab 1 abc 123 Ab2 abbb");
+//        p.pathOfStopWordsToSetOfStrings("C:\\Users\\mohse\\Desktop\\corpusTest\\stopWords.txt");
+         Set<String> setString = p.deleteStopWords("C:\\Users\\mohse\\Desktop\\corpusTest\\stopWords.txt","apple a bannana any wish dad did");
+         System.out.println(setString.toString());
     }
 }
