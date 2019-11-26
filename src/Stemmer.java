b513059d4c1a5a32a5c1bf632e16822d7fdea958
mohsen <mohsen.abdalla.98@gmail.com>
2019-11-26 12:27:59 +0200

    Porter's-Stemmer +stemmer Func

--- src/Stemmer.java
+++ src/Stemmer.java
@@ -1,8 +1,14 @@
 import org.tartarus.snowball.ext.PorterStemmer;
 
-public class Stemmer {
-//            PorterStemmer stemmer = new PorterStemmer();
-//            stemmer.setCurrent(input); //set string you need to stem
-//            stemmer.stem();  //stem the word
-//            stemmer.getCurrent();//get the stemmed word
+public class Stemmer extends PorterStemmer{
+    private PorterStemmer stemmer;
+    public Stemmer(){
+        this.stemmer = new PorterStemmer();
+    }
+
+    public String porterStemmer(String input){
+            stemmer.setCurrent(input); //set string you need to stem
+            stemmer.stem();  //stem the word
+             return stemmer.getCurrent();//get the stemmed words
+    }
 }
