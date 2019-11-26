2827e9bd41e255555a78ed0c016dcfd02c694f7a
Evgeniy Umanskiy <evgeniyu@sensus.co.il>
2019-11-26 12:33:03 +0200

    deleteStopWords returns string

--- src/Stemmer.java
+++ src/Stemmer.java
@@ -1,6 +1,7 @@
 import org.tartarus.snowball.ext.PorterStemmer;
 
 public class Stemmer {
+
 //            PorterStemmer stemmer = new PorterStemmer();
 //            stemmer.setCurrent(input); //set string you need to stem
 //            stemmer.stem();  //stem the word
