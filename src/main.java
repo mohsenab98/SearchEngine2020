907d07c89b2c1c307cbf81cdee800f4c2036461b
Evgeniy Umanskiy <evgeniyu@sensus.co.il>
2019-12-01 23:15:23 +0200

    merge indexer and parser

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -8,27 +9,16 @@ public class main {
         double startTime = System.nanoTime();
 
     /////// ReadFile tests ///////
+       // int k = new File("D:\\corpus").list().length;
+       // int maxRun = k/6;
+
         ReadFile rd = new ReadFile();
-     //   rd.filesSeparator("D:\\corpus");
-     //   rd.filesSeparator("D:\\corpusTestD");
-
-        rd.filesSeparator("C:\\Users\\EvgeniyU\\Desktop\\ThirdYear\\DataRetrieval\\corpusTest");
-
-
-    /////// Parse tests ///////
-
-        Parse p = new Parse(rd.getListAllDocs(), "C:\\Users\\EvgeniyU\\Desktop\\ThirdYear\\DataRetrieval\\corpusTest\\StopWords");
+        //rd.filesSeparator("D:\\corpus");
+        rd.filesSeparator("D:\\corpusTestD");
+        //rd.filesSeparator("C:\\Users\\EvgeniyU\\Desktop\\ThirdYear\\DataRetrieval\\corpusTest");
+        Indexer n = new Indexer();
+        Parse p = new Parse(rd.getListAllDocs(), "C:\\Users\\EvgeniyU\\Desktop\\ThirdYear\\DataRetrieval\\corpusTest\\StopWords", n);
         p.Parser();
-        //p.termIdentifier("123.123.123 100 million");
-        //System.out.println(p.NumWithoutUnits("1010.56"));
-        //System.out.println(p.NumWithPercent("15 percent"));
-        //System.out.println(p.NumWithDates("15 May"));
-        //System.out.println(p.Price("20.6m Dollars"));
-
-        // p.stringToSetOfString("a ab 1 abc 123 Ab2 abbb");
-//        p.pathOfStopWordsToSetOfStrings("C:\\Users\\mohse\\Desktop\\corpusTest\\stopWords.txt");
-         // Set<String> setString = p.deleteStopWords("C:\\Users\\mohse\\Desktop\\corpusTest\\stopWords.txt","apple a bannana any wish dad did");
-         // System.out.println(setString.toString());
 
         double endTime   = System.nanoTime();
         double totalTime = (endTime - startTime) / 1000000000 ;
