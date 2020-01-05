e44cdc576c8a7d85df7e3567c93a997b19ae0299
mohsen <mohsen.abdalla.98@gmail.com>
2020-01-05 13:50:50 +0200

    - V.1 -- Part B

--- src/Classes/Searcher.java
+++ src/Classes/Searcher.java
@@ -1,11 +1,14 @@
 package Classes;
-
 import Model.MyModel;
-
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 /**
  * Dictionary(Search term ) ->>>>>> search in posting(a-z) the relative info of the term
@@ -27,11 +30,54 @@ public class Searcher {
                 continue;
             }
             List<Integer> termInfo = getTermInfo(termLine);
-            String termPostingLine = getPostingLine(termInfo.get(2));
+            String termPostingLine = getPostingLine(termInfo.get(2), termLine.charAt(0));
             Map<Integer,Integer> docTf = getDocTf(termPostingLine); ////????????????????????
 
         }
 
+    }
+
+
+    /**
+     * get the term line info from the dictionary and return the [total appearance(in the corpus) + df + line number]
+     * @param termLine
+     * @return
+     */
+    private List<Integer> getTermInfo(String termLine) {
+        int colonIndex = termLine.indexOf(':');
+        int semicolonIndex = termLine.indexOf(';');
+        List<Integer> listTermInfo = new ArrayList<>();
+
+        //add totalDocs
+        listTermInfo.add(Integer.parseInt(termLine.substring(0, colonIndex)));
+        //add df
+        listTermInfo.add(Integer.parseInt(termLine.substring(colonIndex, semicolonIndex)));
+        //add LineCounter
+        listTermInfo.add(Integer.parseInt(termLine.substring(semicolonIndex)));
+
+        return listTermInfo;
+    }
+
+    /**
+     * receive line number and return the posting line as a String
+     * @param postingLineNumber
+     * @return
+     */
+    private String getPostingLine(int postingLineNumber, char postingName) {
+        int lineCounter = 0;
+        try {
+            Stream<String> lines = Files.lines(Paths.get(this.postingPath + "/" + postingName), StandardCharsets.US_ASCII );
+            // get line [#postingLineNumber] in posting file
+            for( String line : (Iterable<String>) lines::iterator ){
+                if(lineCounter == postingLineNumber){
+                    return line;
+                }
+                lineCounter++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     /**
@@ -40,25 +86,6 @@ public class Searcher {
      * @return
      */
     private Map<Integer, Integer> getDocTf(String termPostingLine) {
-        return null;
-    }
-
-    /**
-     * receive line number and return the posting line as a String
-     * @param integer
-     * @return
-     */
-    private String getPostingLine(int integer) {
-        return null;
-
-    }
-
-    /**
-     * get the term line info from the dictionary and return the [total appearance(in the corpus) + df + line number]
-     * @param termLine
-     * @return
-     */
-    private List<Integer> getTermInfo(String termLine) {
         return null;
     }
 
