707a4cb8fc5b69d46c0d5a694d562a74832980f0
Evgeniy Umanskiy <evgeniyu@sensus.co.il>
2020-01-07 15:18:01 +0200

    -- V.1 Part B - Searcher BM25

--- src/Classes/Ranker.java
+++ src/Classes/Ranker.java
@@ -1,4 +1,26 @@
 package Classes;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Ranker {
+    private int N;
+    private int avgdl;
+
+    public Ranker(int n, int avgdl) {
+        N = n;
+        this.avgdl = avgdl;
+    }
+
+
+
+    /**
+     * gets Doc(key) -- (qi - tfi - dfi)(value)
+     * @param docQ
+     * @return docID - score
+     */
+    public Map<Integer, Integer> rankBM25(Map<Integer, ArrayList<String>> docQ) {
+
+        return null;
+    }
 }
